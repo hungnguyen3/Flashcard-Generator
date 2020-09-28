685ad57d5f7b3eebdbb9374a1ab679e3dd7a9265
daniel passy <danielpassy@hotmail.com>
2020-09-28 19:32:19 -0300

    "fixed nav bar not collapsing"

--- src/components/NavbarContainer.js
+++ src/components/NavbarContainer.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../static/navbar.css";
 
 function NavbarContainer() {
+	
+	// this is a hook, it adds "state" to a functional component
+	const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+	// flip state
+	const handleNavCollapse = () => {
+		setIsNavCollapsed(!isNavCollapsed)
+	};
+
+
 	return (
 		<nav class="navbar navbar-expand-md navbar-dark bg-dark">
 			<a class="navbar-brand mr-auto" href="/">
@@ -12,10 +22,13 @@ function NavbarContainer() {
 				type="button"
 				data-toggle="collapse"
 				data-target="#navbarNav"
+				onClick={handleNavCollapse}
 			>
 				<span class="navbar-toggler-icon"></span>
 			</button>
-			<div class="navbar-collapse collapse" id="navbarNav">
+
+			{/* renders the nav bar according to the state */}
+			<div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
 				<ul class="navbar-nav ml-auto">
 					<li class="nav-item">
 						<a class="nav-link" href="/Examples">
